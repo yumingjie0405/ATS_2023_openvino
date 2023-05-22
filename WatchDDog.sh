53be9182fdd69594977f0d51dabed8028e74a5e7
ymj <1465739029@qq.com>
2023-05-22 21:05:43 +0800

    add

--- WatchDDog.sh
+++ WatchDDog.sh
@@ -1,31 +1,26 @@
-#!/bin/bash 
+#!/bin/bash
 
+cd /home/ymj/Desktop/TUP-InfantryVision-2022/build
 source /opt/intel/openvino_2021/bin/setupvars.sh
-sec=2 
-cnt=0 
-name=TUP-InfantryVision-2022-main
-program_name=Infantry_Vision
-cd /home/tup/Desktop/$name/build/
-#make clean && 
-make -j6 
-while [ 1 ] 
-do 
-    count=`ps -ef | grep $program_name | grep -v "grep" | wc -l`
-    echo "Thread count: $count" 
-    echo "Expection count: $cnt" 
-    if [ $count -ge 1 ]; then 
-        echo "The $name is still alive!" 
-        sleep $sec 
-    else  
-        echo "Starting $name..." 
-        gnome-terminal -- bash -c "cd /home/tup/Desktop/$program_name/build/;
-        ./$program_name;exec bash;" 
-        echo "$name has started!"   
-        ((cnt=cnt+1)) 
-        sleep $sec 
-        if [ $cnt -gt 9 ]; then 
-            echo "Reboot!" 
-            #reboot 
-        fi 
-    fi 
+./run
+while true; do
+    # 检查是否存在名为 "run" 的进程
+    if pgrep run >/dev/null 2>&1 ; then
+        sleep 1
+    else
+        echo "Process is not running. Restarting..."
+        #make clean && make -j6
+        make -j6
+        gnome-terminal -- bash -c "./run; exec bash;"
+        # 在这里添加你需要执行的命令，比如重启你的程序
+    fi
+
+    # 检查系统日志以查看是否有段错误（SIGSEGV）发生
+    if dmesg | grep -q "run.*\[sig\]"; then
+        echo "Segmentation fault detected. Restarting..."
+#        make clean && make -j6
+        make -j6
+        gnome-terminal -- bash -c "./run; exec bash;"
+        # 在这里添加你需要执行的命令，比如重启你的程序
+    fi
 done
